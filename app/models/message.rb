c304d4cdb793c91f5191004209d5cc9efd8c0070
chanhito05 <koumeto0511@gmail.com>
2024-03-02 17:43:04 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room # チャットルームに存在します
   belongs_to :user # 一人のユーザーから送信されます
+  has_one_attached :image
 
   validates :content, presence: true # 空の場合はDBに保存しない
 end
